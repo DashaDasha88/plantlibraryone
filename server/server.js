0168b91f1316390af0240b3c38a69be543312c17
Dasha <dasha.kotova.mail@gmail.com>
2021-05-28 13:51:26 -0400

    add restful api skeletons

--- server/server.js
+++ server/server.js
@@ -1,10 +1,70 @@
 require("dotenv").config();
 const express = require("express");
+const db = require("./db");
 
 const app = express();
 
-console.log("kittycat");
+app.use(exprss.json()); //middleware
 
+//GET all plants
+app.get("/plants", async (req,res) => {
+
+  try {
+    const results = await db.query();
+
+  } catch (err) {
+    console.log(err);
+  };
+
+});
+
+//GET a specific plant
+app.get("/plants/:id", async (req, res) => {
+
+  try {
+    const results = await db.query();
+
+  } catch (err) {
+    console.log(err);
+  };
+
+});
+
+//CREATE a plant
+app.post("/plants", async (req, res) => {
+
+  try {
+    const results = await db.query();
+
+  } catch (err) {
+    console.log(err);
+  };
+
+});
+
+//UPDATE a plant
+app.put("/plants/:id", async (req, res) => {
+
+  try {
+    const results = await db.query();
+
+  } catch (err) {
+    console.log(err);
+  };
+
+});
+
+//DELETE a plant
+app.delete("/plants/:id", async (req, res) => {
+
+  try {
+    const results = await db.query();
+
+  } catch (err) {
+    console.log(err);
+  };
+
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
